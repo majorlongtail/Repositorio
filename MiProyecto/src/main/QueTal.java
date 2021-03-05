db1331fe0d0fca2ee1b22b987150d322058e87fc
majorlongtail <generalrabogolfo@gmail.com>
2021-03-05 09:50:46 +0100

    lmao

--- MiProyecto/src/main/QueTal.java
+++ MiProyecto/src/main/QueTal.java
@@ -3,5 +3,6 @@ public class QueTal {
 	public static void main(String [] args) {
 		System.out.println("que tal bro");
 		System.out.println("que tal brooooo");
+		System.out.println("na bro aqui");
 	}
 }
